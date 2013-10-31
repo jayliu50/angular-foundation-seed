d9e48699404ad82fbf787c68210edbc61920f2e8
cyanos3 <jayliu50@gmail.com>
2013-10-30 18:02:10 -0600

    Initial Commit!

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('kinpointApp')
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'jQuery',
+            'Font Awesome',
+            'Zurb Foundation',
+            'Compass',
+            'AngularJS',
+            'Karma'
+        ];
+    });
